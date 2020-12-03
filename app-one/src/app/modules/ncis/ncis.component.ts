ed5da1ada3a2a9ddc55a87b509e413770eb852b2
Kiran J. Holla <kjholla@gmail.com>
2020-12-03 16:41:38 +0530

    Fixed typo in the Component selector.

--- app-one/src/app/modules/ncis/ncis.component.ts
+++ app-one/src/app/modules/ncis/ncis.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nics',
+  selector: 'app-ncis',
   templateUrl: './ncis.component.html',
   styleUrls: ['./ncis.component.scss']
 })
